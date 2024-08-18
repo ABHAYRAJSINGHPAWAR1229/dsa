c183d383622b3884534b0d8ed6bf5d29538cb07b
Abhay <abhayrajsingh@gmail.com>
2024-08-18 12:14:34 +0530

    Functional Interface

---
+++ FunctionalInterface/Calculator1/Calculator.java
@@ -0,0 +1,41 @@
+
+
+interface A{
+    public void Add();
+}
+
+interface S{
+    public void Subtract();
+}
+
+interface M{
+    public void Multiply();
+}
+
+interface D{
+    public void Divide();
+}
+// class Calculator{
+//     public static void main(String []args){
+//         A a=new A(){
+//             public void Add(){
+//                 System.out.println("Addition");
+//         };
+//     };
+//     a.Add();
+//     }
+// }
+
+class Calculator{
+    public static void main(String []args){
+        A a=()->System.out.println("Addition");
+        S b=()->System.out.println("Subtraction");
+        M c=()->System.out.println("Multiplication");
+        D d=()->System.out.println("Division");
+          
+    a.Add();
+    b.Subtract();
+    c.Multiply();
+    d.Divide();
+    }
+}
